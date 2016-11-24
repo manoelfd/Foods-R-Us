f19c3cf5d749442f3366f062272669881de36411
user <user@eecs-vbox-common.cs.yorku.ca>
2016-11-24 04:52:35 -0500

    base template. shopping cart in model (TODO). next is listing of catalog

---
+++ FoodRUs/src/ctrl/Main.java
@@ -0,0 +1,63 @@
+package ctrl;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.ShoppingCart;
+
+/**
+ * Servlet implementation class Main
+ */
+@WebServlet("/Main")
+public class Main extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Main()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// TODO Auto-generated method stub
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		processRequest(request, response);
+	}
+
+	// have all servlets ovveride this method. This class (main) that servlets
+	// will inherit from will take care of common checks.
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// check if a shopping cart is set for this user
+		HttpSession session = request.getSession();
+
+		if (session.getAttribute("shoppingcart") == null)
+		{ // shopping cart doesn't exist
+			session.setAttribute("shoppingcart", new ShoppingCart());
+		}
+		this.getServletContext().getRequestDispatcher("/pages/home.jspx").forward(request, response);
+	}
+
+}
