f7c08f8a7ce41e389fb26a97f8598c08397530c0
user <user@eecs-vbox-common.cs.yorku.ca>
2016-11-24 17:29:13 -0500

    catalog

---
+++ FoodRUs/src/model/CatalogModel.java
@@ -0,0 +1,12 @@
+package model;
+
+import daos.CategoryDAO;
+import daos.ItemDAO;
+
+public class CatalogModel
+{
+	private CategoryDAO categoryDAO;
+	private ItemDAO itemDAO;
+	
+	//public List<Cate>getAllCategories()
+}
