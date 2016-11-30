253b30eb4e5953ea9b71d5ec68d569f5383c1590
user <user@localhost>
2016-11-29 22:03:09 -0500

    New controller to take care of "addItem" request.

---
+++ FoodRUs/src/ctrl/AddItemController.java
@@ -0,0 +1,87 @@
+package ctrl;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map;
+
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.ShoppingCart;
+
+
+import beans.Item;
+import daos.ItemDAO;
+
+/**
+ * Servlet implementation class AddItemController
+ */
+@WebServlet("/AddItemController")
+public class AddItemController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddItemController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Item item = null;
+		Item item2 = null;
+		Item item3 = null;
+		//String qty = request.getParameter("quantity");
+		//request.setAttribute("quantity", qty);
+		String view = "pages/home.jspx";
+		try {
+			String number = request.getParameter("number");
+			System.out.println("Item number: " + number);
+			item  = new ItemDAO().getItemById("2910h074");
+			item2  = new ItemDAO().getItemById("2910h019");
+			item3 = new ItemDAO().getItemById("2910h087");
+
+		} catch (SQLException | NamingException e) {
+			throw new ServletException("Could not get item with item number ["+
+					request.getParameter("number")+"]", e);
+		} 
+
+		if (item != null) {
+			ShoppingCart cart = (ShoppingCart) request.getSession().getAttribute("shoppingcart");
+			cart.addItem(item);
+			cart.addItem(item2);
+			cart.addItem(item3);
+			
+			Map<Item, Integer> items = cart.getShoppingCart();
+			request.setAttribute("ItemsMap", items);
+			System.out.println("ItemsMap: " + request.getAttribute("ItemsMap"));
+			
+			double subtotal = cart.computeSubTotal();
+			request.setAttribute("subtotal", subtotal);
+			System.out.println("Subtotal: " + request.getAttribute("subtotal"));
+			cart.printCart();
+			view = "pages/cart.jspx";
+		}
+		
+		this.getServletContext().getRequestDispatcher("/" + view).forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
