c43148c5abd75228d6a082f7ae90acf4cafbb28c
manoelfd <manoelfd1@hotmail.com>
2016-12-01 17:58:39 -0500

    Header title shows current page. Fixed login/logout button

--- FoodRUs/src/ctrl/LogInController.java
+++ FoodRUs/src/ctrl/LogInController.java
@@ -15,7 +15,7 @@ import model.CatalogModel;
  * Servlet implementation class Login
  */
 @WebServlet(urlPatterns =
-{ "/Login" , "/Logout"})
+{ "/Login", "/Logout" })
 public class LogInController extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
@@ -30,7 +30,7 @@ public class LogInController extends HttpServlet
 		// this is a test comment
 		// hi
 		// Hello
-		//Project done
+		// Project done
 	}
 
 	/**
@@ -39,43 +39,56 @@ public class LogInController extends HttpServlet
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		//System.out.println("loggedIn1 @request: " + request.getAttribute("loggedIn"));
-		//System.out.println("loggedIn1 @session: " + request.getSession().getAttribute("loggedIn"));
-		//System.out.println("Catalog1 @request: " + request.getAttribute("catalog"));
-		
+		// System.out.println("loggedIn1 @request: " +
+		// request.getAttribute("loggedIn"));
+		// System.out.println("loggedIn1 @session: " +
+		// request.getSession().getAttribute("loggedIn"));
+		// System.out.println("Catalog1 @request: " +
+		// request.getAttribute("catalog"));
+
 		String target = null;
 
-		
-		if(request.getParameter("hash") == null && request.getSession().getAttribute("loggedIn") == null)
+		if (request.getParameter("hash") == null && request.getSession().getAttribute("loggedIn") == null)
 		{
 			target = (String) request.getAttribute("target");
-			//System.out.println("target1: " + target);
-			
+			// System.out.println("target1: " + target);
+
 			String me = request.getRequestURL().toString();
 			String oauth = "https://www.eecs.yorku.ca/~cse31020/auth/AuthProject.cgi?back=";
 			response.sendRedirect(oauth + me);
 			initiateCatalog(request);
-		}
-		else{
-			if(request.getSession().getAttribute("loggedIn") == null){
-				request.getSession().setAttribute("hash", request.getParameter("hash"));// for further verification
-				
+		} else
+		{
+			if (request.getSession().getAttribute("loggedIn") == null)
+			{
+				request.getSession().setAttribute("hash", request.getParameter("hash"));// for
+																						// further
+																						// verification
+
 				request.getSession().setAttribute("loggedIn", request.getParameter("user"));
 				request.setAttribute("loggedIn", request.getParameter("user"));
-			}else{
+			} else
+			{
 				request.setAttribute("loggedIn", request.getSession().getAttribute("loggedIn"));
 			}
-			
-			if(request.getParameter("signout") != null && request.getParameter("signout").equals("Sign Out")){
+			System.out.println(request.getServletPath());
+			// if(request.getParameter("signout") != null &&
+			// request.getParameter("signout").equals("Sign Out")){
+			if (request.getServletPath().equals("/Logout"))
+			{
+
 				request.getSession().removeAttribute("loggedIn");
-				//page = "page/home.jspx";// or logout page
+				// page = "page/home.jspx";// or logout page
 			}
 
-			//System.out.println("loggedIn2 @request: " + request.getAttribute("loggedIn"));
-			//System.out.println("loggedIn2 @session: " + request.getSession().getAttribute("loggedIn"));
-			//System.out.println("target2: " + request.getAttribute("target"));
-			//System.out.println("Catalog2 @request: " + request.getAttribute("catalog"));
-			
+			// System.out.println("loggedIn2 @request: " +
+			// request.getAttribute("loggedIn"));
+			// System.out.println("loggedIn2 @session: " +
+			// request.getSession().getAttribute("loggedIn"));
+			// System.out.println("target2: " + request.getAttribute("target"));
+			// System.out.println("Catalog2 @request: " +
+			// request.getAttribute("catalog"));
+
 			initiateCatalog(request);
 			this.getServletContext().getRequestDispatcher("/pages/home.jspx").forward(request, response);
 		}
@@ -86,13 +99,13 @@ public class LogInController extends HttpServlet
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		doGet(request, response);
 	}
-	
-	protected void initiateCatalog(HttpServletRequest request){
+
+	protected void initiateCatalog(HttpServletRequest request)
+	{
 		CatalogModel cataModel = (CatalogModel) getServletContext().getAttribute("catalogModel");
 		request.setAttribute("catalog", cataModel.getCatalog());
 	}
--- FoodRUs/src/ctrl/Main.java
+++ FoodRUs/src/ctrl/Main.java
@@ -16,7 +16,7 @@ import model.ShoppingCart;
 /**
  * Servlet implementation class Main
  */
-@WebServlet("/Main")
+@WebServlet({"/Main"})
 public class Main extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
