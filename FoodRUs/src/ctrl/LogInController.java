5fda3ee57f05dcae252a62f9fc916d57d16419d6
manoelfd <manoelfd1@hotmail.com>
2016-12-04 17:14:51 -0500

    restructured login controller. it can also forward back to the calling servlet.

--- FoodRUs/src/ctrl/LogInController.java
+++ FoodRUs/src/ctrl/LogInController.java
@@ -2,6 +2,7 @@ package ctrl;
 
 import java.io.IOException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -45,52 +46,86 @@ public class LogInController extends HttpServlet
 		// request.getSession().getAttribute("loggedIn"));
 		// System.out.println("Catalog1 @request: " +
 		// request.getAttribute("catalog"));
+		String username = request.getParameter("user");
+		String hash = request.getParameter("hash");
+		String ref = request.getParameter("ref");
 
 		String target = null;
 
-		if (request.getParameter("hash") == null && request.getSession().getAttribute("loggedIn") == null)
+		// if (request.getParameter("hash") == null &&
+		// request.getSession().getAttribute("loggedIn") == null)
+		// {
+		// target = (String) request.getAttribute("target");
+		// // System.out.println("target1: " + target);
+		//
+		// String me = request.getRequestURL().toString();
+		// String oauth =
+		// "https://www.eecs.yorku.ca/~cse23116/auth/Auth.cgi?back=";
+		// response.sendRedirect(oauth + me);
+		// initiateCatalog(request);
+		// } else
+		if (username != null && hash != null)
 		{
-			target = (String) request.getAttribute("target");
-			// System.out.println("target1: " + target);
+			try
+			{
+				String usernameHash = javax.xml.bind.DatatypeConverter.printHexBinary(
+						MessageDigest.getInstance("SHA1").digest((username + "paranoidandroid").getBytes()));
+				System.out.println("hash: " + hash);
+				System.out.println("username hash: " + usernameHash);
+				if (hash.equalsIgnoreCase(usernameHash))
+				{
+					System.out.println("User auth'd succesfully!");
+					request.getSession().setAttribute("loggedIn", username);
+				} else
+				{
+					request.getSession().removeAttribute("loggedIn");
+				}
+			} catch (NoSuchAlgorithmException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-			String me = request.getRequestURL().toString();
-			String oauth = "https://www.eecs.yorku.ca/~cse31020/auth/AuthProject.cgi?back=";
-			response.sendRedirect(oauth + me);
-			initiateCatalog(request);
+		}
+		System.out.println(request.getServletPath());
+		// if(request.getParameter("signout") != null &&
+		// request.getParameter("signout").equals("Sign Out")){
+		if (request.getServletPath().equals("/Logout"))
+		{
+			request.getSession().removeAttribute("loggedIn");
+			initiateCatalog(request); // TODO-- this is bad
+			request.setAttribute("target", "Catalog.jspx");
+			this.getServletContext().getRequestDispatcher("/pages/home.jspx").forward(request, response);
+			return;
+			// page = "page/home.jspx";// or logout page
+		}
+
+		// System.out.println("loggedIn2 @request: " +
+		// request.getAttribute("loggedIn"));
+		// System.out.println("loggedIn2 @session: " +
+		// request.getSession().getAttribute("loggedIn"));
+		// System.out.println("target2: " + request.getAttribute("target"));
+		// System.out.println("Catalog2 @request: " +
+		// request.getAttribute("catalog"));
+
+		initiateCatalog(request);
+		if (request.getSession().getAttribute("loggedIn") != null)
+		{
+			if (ref != null)
+			{
+				//System.out.println("ref: " + ref);
+				this.getServletContext().getRequestDispatcher(ref).forward(request, response);
+				// response.sendRedirect(request.getContextPath()+ref);
+			} else
+				this.getServletContext().getRequestDispatcher("/pages/home.jspx").forward(request, response);
 		} else
 		{
-			if (request.getSession().getAttribute("loggedIn") == null)
-			{
-				request.getSession().setAttribute("hash", request.getParameter("hash"));// for
-																						// further
-																						// verification
-
-				request.getSession().setAttribute("loggedIn", request.getParameter("user"));
-				request.setAttribute("loggedIn", request.getParameter("user"));
-			} else
-			{
-				request.setAttribute("loggedIn", request.getSession().getAttribute("loggedIn"));
-			}
-			System.out.println(request.getServletPath());
-			// if(request.getParameter("signout") != null &&
-			// request.getParameter("signout").equals("Sign Out")){
-			if (request.getServletPath().equals("/Logout"))
-			{
-
-				request.getSession().removeAttribute("loggedIn");
-				// page = "page/home.jspx";// or logout page
-			}
-
-			// System.out.println("loggedIn2 @request: " +
-			// request.getAttribute("loggedIn"));
-			// System.out.println("loggedIn2 @session: " +
-			// request.getSession().getAttribute("loggedIn"));
-			// System.out.println("target2: " + request.getAttribute("target"));
-			// System.out.println("Catalog2 @request: " +
-			// request.getAttribute("catalog"));
-
-			initiateCatalog(request);
-			this.getServletContext().getRequestDispatcher("/pages/home.jspx").forward(request, response);
+			String me = request.getRequestURL().toString();
+			String reff = (String) request.getAttribute("ref");
+			String oauth = "https://www.eecs.yorku.ca/~cse23116/auth/Auth.cgi?back=" + me
+					+ (reff == null ? "" : "&ref=" + reff);
+			//System.out.println(oauth);
+			response.sendRedirect(oauth);
 		}
 
 	}
