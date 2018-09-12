2aa48b7b1ebcbe5b130f6ad289e6df3f58bbd87e
Manoel Farhad <manoelfd@users.noreply.github.com>
2018-09-12 17:30:14 -0400

    cleanup

--- FoodRUs/src/Tag/PictureTag.java
+++ FoodRUs/src/Tag/PictureTag.java
@@ -13,15 +13,7 @@ public class PictureTag extends SimpleTagSupport
     {
             JspWriter os = this.getJspContext().getOut();
             StringWriter sw = new StringWriter();
-            //getJspBody().invoke(sw);
-
             PageContext pC = (PageContext)getJspContext();
-
-            //os.write("<img src='/FoodRUs/res/img/project/"+this.categoryID +".png" + "'/>");
-    		//os.write("<img src=\"" + pC.getServletContext().getRealPath("/res/img/project/"+this.categoryID +".png") + "\"/>");
-            //reak path was not working
-            //instead of <img src="${pageContext.request.contextPath}/res/img/man/polo-shirt-2.png" alt="polo shirt img" />
-
     }
     public void setcategoryID(String categoryID)
     {
